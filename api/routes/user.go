d375f0ae7278289ba535891cf16a697bf214d418
MohamedElsayed2020 <https://github.com/Mohamedelsayed2020/UserApi.git>
2020-05-22 03:32:40 +0200

    chatting using websocket

--- api/routes/user.go
+++ api/routes/user.go
@@ -2,19 +2,23 @@ package routes
 
 import (
 	"LoginAndChatTask/api/controllers"
+	"LoginAndChatTask/model/common"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 func Routes()  {
 
 	r := mux.NewRouter()
+	sr := http.FileServer(http.Dir("../temp"))
+
 	user := controllers.UserController{}
 	r.HandleFunc("/user", user.CreateUser).Methods("POST")
 	r.HandleFunc("/user", user.ListUsers).Methods("Get")
 	r.HandleFunc("/user/{id}", user.GetUser).Methods("Get")
 	r.HandleFunc("/user/{id}", user.UpdateUser).Methods("PUT")
 	r.HandleFunc("/user/login", user.Login).Methods("POST")
+	http.Handle("/", sr)
+	http.HandleFunc("/ws", common.HandleConnections)
 
-
-	http.ListenAndServe(":8000", r)
+	http.ListenAndServe(":8080", r)
 }
